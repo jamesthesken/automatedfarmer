cc85a50c7d400e9c7153770875683452e0eb000c
jamesthesken <jthesken@hawaii.edu>
2020-06-04 23:09:18 -1000

    Add CO2 sensor code and clean up dashboard a bit

--- src/examples/dhtServer.ino
+++ src/examples/dhtServer.ino
@@ -22,19 +22,26 @@
 DHT_Unified dht(DHTPIN, DHTTYPE);
 uint32_t delayMS;
 
+#include <SoftwareSerial.h>                           //we have to include the SoftwareSerial library, or else we can't use it
+#define rx 5                                          //define what pin rx is going to be
+#define tx 4                                          //define what pin tx is going to be
+SoftwareSerial myserial(rx, tx);                      //define how the soft serial port is going to work
+
 // To avoid hardcoded WiFi credentials: https://github.com/alanswx/ESPAsyncWiFiManager
 #include <ESPAsyncWiFiManager.h> 
-
 
 AsyncWebServer server(80);
 DNSServer dns;
 
+
+
 // Get temperature event and print its value.
 float readDHTemperatureC() {
-  // set placeholder value
-  float tempC = 0.0;
   sensors_event_t event;
   dht.temperature().getEvent(&event);
+
+  float tempC = event.temperature;
+  
   if (isnan(event.temperature)) {
     Serial.println(F("Error reading temperature!"));
   }
@@ -44,12 +51,67 @@ float readDHTemperatureC() {
     Serial.println(F("°C"));
     float tempC = event.temperature;
   }
+
+    Serial.print(F("Temperature: "));
+    Serial.print(event.temperature);
+    Serial.println(F("°C"));  
+
   return float(tempC);
 }
 
+String inputstring = "";                              //a string to hold incoming data from the PC
+String sensorstring = "";                             //a string to hold the data from the Atlas Scientific product
+boolean input_string_complete = false;                //have we received all the data from the PC
+boolean sensor_string_complete = false;               //have we received all the data from the Atlas Scientific product
+int Co2;                                              //used to hold a integer number that is the Co2
+
+void serialEvent() {                                  //if the hardware serial port_0 receives a char
+  inputstring = Serial.readStringUntil(13);           //read the string until we see a <CR>
+  input_string_complete = true;                       //set the flag used to tell if we have received a completed string from the PC
+}
+
+int co2Reading(){
+  if (input_string_complete == true) {                //if a string from the PC has been received in its entirety
+    myserial.print(inputstring);                      //send that string to the Atlas Scientific product
+    myserial.print('\r');                             //add a <CR> to the end of the string
+    inputstring = "";                                 //clear the string
+    input_string_complete = false;                    //reset the flag used to tell if we have received a completed string from the PC
+  }
+
+  if (myserial.available() > 0) {                     //if we see that the Atlas Scientific product has sent a character
+    char inchar = (char)myserial.read();              //get the char we just received
+    sensorstring += inchar;                           //add the char to the var called sensorstring
+    if (inchar == '\r') {                             //if the incoming character is a <CR>
+      sensor_string_complete = true;                  //set the flag
+    }
+  }
+
+
+  if (sensor_string_complete == true) {               //if a string from the Atlas Scientific product has been received in its entirety
+    Serial.println(sensorstring);                     //send that string to the PC's serial monitor
+                                                    //uncomment this section to see how to convert the Co2 readings from a string to an  integer 
+    if (isdigit(sensorstring[0])) {                   //if the first character in the string is a digit
+      Co2 = sensorstring.toInt();                     //convert the string to a integer so it can be evaluated by the Arduino
+      if (Co2 >= 400) {                               //if the Co2 is greater than or equal to 400
+        Serial.println("high");                       //print "high" this is demonstrating that the Arduino is evaluating the Co2 as a number and not as a string
+      }
+      if (Co2 <= 399) {                               //if the Co2 is less than or equal to 399
+        Serial.println("low");                        //print "low" this is demonstrating that the Arduino is evaluating the Co2 as a number and not as a string
+      }
+    }
+    
+    sensorstring = "";                                //clear the string
+    sensor_string_complete = false;                   //reset the flag used to tell if we have received a completed string from the Atlas Scientific product
+  }
+
+  return Co2;
+}
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
+  myserial.begin(9600);
+  inputstring.reserve(10);                            //set aside some bytes for receiving data from the PC
+  sensorstring.reserve(30);
 
   // Initialize the sensor
   dht.begin();
@@ -97,8 +159,8 @@ void setup() {
 
   ESPDash.init(server);   // Initiate ESPDash and attach your Async webserver instance
   // Add Respective Cards
-  ESPDash.addNumberCard("num1", "pH", 264);
-  ESPDash.addTemperatureCard("temp1", "Water Temperature", 0, 20);
+  ESPDash.addTemperatureCard("temp1", "Air Temperature", 0, 20);
+  ESPDash.addNumberCard("num1", "CO2 (ppm)", 264);
 
   server.begin();
   
@@ -111,6 +173,7 @@ void loop() {
     
   Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
+  ESPDash.updateNumberCard("num1", co2Reading());
   ESPDash.updateTemperatureCard("temp1", readDHTemperatureC());
   delay(3000);
 }
