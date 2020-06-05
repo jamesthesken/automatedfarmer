eada8fe0c3f292ceb9b0063dfff938d5f36b22d4
jamesthesken <jthesken@hawaii.edu>
2020-06-04 18:20:36 -1000

    Add readDHT function

--- src/examples/dhtServer.ino
+++ src/examples/dhtServer.ino
@@ -17,6 +17,8 @@
 #include <DHT.h>
 #include <DHT_U.h>
 
+#define DHTPIN 2     // NodeMCU Pin D4
+#define DHTTYPE    DHT11     // DHT 11
 DHT_Unified dht(DHTPIN, DHTTYPE);
 uint32_t delayMS;
 
@@ -27,17 +29,20 @@ uint32_t delayMS;
 AsyncWebServer server(80);
 DNSServer dns;
 
+// Get temperature event and print its value.
 float readDHTemperatureC() {
-  // Call sensors.requestTemperatures() to issue a global temperature and Requests to all devices on the bus
-  sensors.requestTemperatures(); 
-  float tempC = sensors.getTempCByIndex(0);
-
-  if(tempC == -127.00) {
-    Serial.println("Failed to read from DS18B20 sensor");
-    return 0.0;
-  } else {
-    Serial.print("Temperature Celsius: ");
-    Serial.println(tempC); 
+  // set placeholder value
+  float tempC = 0.0;
+  sensors_event_t event;
+  dht.temperature().getEvent(&event);
+  if (isnan(event.temperature)) {
+    Serial.println(F("Error reading temperature!"));
+  }
+  else {
+    Serial.print(F("Temperature: "));
+    Serial.print(event.temperature);
+    Serial.println(F("°C"));
+    float tempC = event.temperature;
   }
   return float(tempC);
 }
@@ -48,6 +53,30 @@ void setup() {
 
   // Initialize the sensor
   dht.begin();
+  // Print temperature sensor details.
+  sensor_t sensor;
+  dht.temperature().getSensor(&sensor);
+  Serial.println(F("------------------------------------"));
+  Serial.println(F("Temperature Sensor"));
+  Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
+  Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
+  Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
+  Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("°C"));
+  Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("°C"));
+  Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("°C"));
+  Serial.println(F("------------------------------------"));
+  // Print humidity sensor details.
+  dht.humidity().getSensor(&sensor);
+  Serial.println(F("Humidity Sensor"));
+  Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
+  Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
+  Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
+  Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("%"));
+  Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("%"));
+  Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("%"));
+  Serial.println(F("------------------------------------"));
+  // Set delay between sensor readings based on sensor details.
+  delayMS = sensor.min_delay / 1000;
 
   AsyncWiFiManager wifiManager(&server,&dns);
 
